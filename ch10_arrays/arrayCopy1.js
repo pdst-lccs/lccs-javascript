ca2666637b7b3228812dc66a9adabe3ea984b929
pdst-lccs <50370310+pdst-lccs@users.noreply.github.com>
2021-05-31 13:22:06 +0100

    Update arrayCopy1.js

--- ch10_arrays/arrayCopy1.js
+++ ch10_arrays/arrayCopy1.js
@@ -1,11 +1,6 @@
 let arr1 = [1, 3, 5, 7, 9];
 let arr2 = [];
 
-console.log("BEFORE COPY");
-console.log("Array 1:", arr1);
-console.log("Array 2:", arr2);
-
-
 for (let index = 0; index < arr1.length; index++) {
     arr2[index] = arr1[index];
 }
